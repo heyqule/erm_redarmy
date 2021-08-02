f4c032b9feb787e09044e556808ffa5ec4f4182a
heyqule <hq.jfax@gmail.com>
2021-08-02 08:56:30 -0400

    0.4.0
    Reduced unit vision to 30 - 35

--- prototypes/enemy/human-sniper.lua
+++ prototypes/enemy/human-sniper.lua
@@ -65,7 +65,7 @@ local base_movement_speed = 0.075
 local incremental_movement_speed = 0.1
 
 -- Misc settings
-local vision_distance = 40
+local vision_distance = 30
 
 local pollution_to_join_attack = 120
 local distraction_cooldown = 20
