d283fdf073d3c950bca618a3c8e043082c124eff
heyqule <hq.jfax@gmail.com>
2022-07-10 01:18:47 -0400

    - Improve unit performance by changing enemies distraction_cooldown from 20 to 300.

--- prototypes/enemy/human-pistol.lua
+++ prototypes/enemy/human-pistol.lua
@@ -60,7 +60,7 @@ local incremental_movement_speed = 0.1
 local vision_distance = 30
 
 local pollution_to_join_attack = 5
-local distraction_cooldown = 20
+local distraction_cooldown = 300
 
 -- Animation Settings
 local unit_scale = 1
